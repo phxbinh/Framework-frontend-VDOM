45f45e0c7eee85a220fb038d22d2f90d45057750
phxbinh <phxbinh@gmail.com>
2026-01-07 11:05:35 +0700

    Add files via upload

---
+++ initialState/initialState.js
@@ -0,0 +1,12 @@
+// ==========================
+// 1) initialState
+// ==========================
+const initialState = {
+  auth: {
+    user: null, token: null, role: null,
+    email: null, avatar: null,
+    lastLogin: null
+  },
+  counter: { value: 0 },
+  postsA: { postsDemo: [] }
+};
